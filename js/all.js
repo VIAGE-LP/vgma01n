90e91a33768b9e3603b9f80d43bd7062ce9d556e
Pickachu Chen <pickachu1006@gmail.com>
2021-04-08 17:43:12 +0800

    20210408_anniversary update

--- js/all.js
+++ js/all.js
@@ -27,27 +27,15 @@ $(document).ready(function () {
         $(".line_link").attr("href", "https://line.me/R/ti/p/%40fhl1857j");
     }
 
-
-});
-
-$("#lp_form").ready(function () {
-    //購物車下拉選單判斷預購
-    $('select[data-category="內衣"][data-name="size-selector"], select[data-category="內衣"][name="color-selector"]').change(function (e) {
+    //週年慶活動辦法下拉選單
+    $(".anniversary_2,.SP_anniversary_2").hide();
+    $('.anniversary_1, .SP_anniversary_1').click(function (e) { 
         e.preventDefault();
-        var size_selector = document.querySelector('select[data-category="內衣"][data-name="size-selector"]').value;
-        var color_selector = document.querySelector('select[data-category="內衣"][name="color-selector"]').value;
-        var preorder_text = $('<span>', { text: '(預購)', style: 'color: red;' });
-        $('.form-group > span').empty();
-        if ((color_selector == "天空藍" && size_selector == "M") || (color_selector == "櫻花粉" && size_selector == "M") || (color_selector == "櫻花粉" && size_selector == "ML") || (color_selector == "櫻花粉" && size_selector == "SM") || (color_selector == "海軍藍" && size_selector == "M") || (color_selector == "薰衣草紫" && size_selector == "M") || (color_selector == "寧靜灰" && size_selector == "S") || (color_selector == "異色黑" && size_selector == "S") || (color_selector == "異色黑" && size_selector == "ML")) {
-            $('select[data-name="size-selector"]').parent().after(preorder_text);
-        } else {
-            $('.form-group > span').empty();
-        }
+        $(".anniversary_2,.SP_anniversary_2").toggle();;
     });
-    // 天空藍：M
-    // 櫻花粉：M、ML、SM
-    // 海軍藍：M
-    // 薰衣草紫：M
-    // 寧靜灰: S
-    // 異色黑: S、ML
+    $('.anniversary_2, .SP_anniversary_2').click(function (e) { 
+        e.preventDefault();
+        $(".anniversary_2, .SP_anniversary_2").hide();
+    });
+
 });
