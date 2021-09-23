09bdd252bc8baad422466479a20ba90bce7b59a1
VIAGE-LP <57522097+VIAGE-LP@users.noreply.github.com>
2021-09-23 12:54:16 +0800

    210922
    210922

--- js/all.js
+++ js/all.js
@@ -5,49 +5,36 @@ $(document).ready(function () {
     });
     observer.observe();
     // 立即購買btn動態
-    $('.offerbtn, .offerbtn_m').click(function (e) {
+    $('.offerbtn, .offerbtn_m').click(function (e) { 
         e.preventDefault();
         $('html , body').animate({
             scrollTop: $('#size').offset().top,
-        }, 900);
+        },900);
     });
     // 立即訂購btn動態
-    $('.buybtn,a[href="#size"]').click(function (e) {
+    $('.buybtn,a[href="#size"]').click(function (e) { 
         e.preventDefault();
         $('html , body').animate({
             scrollTop: $('#size').offset().top,
-        }, 900);
+        },900);
     });
 
     //偵測sns line btn 手機版還是電腦版，連結不同
-    var vw = $(window).width();
+    var vw=$(window).width();
     if (vw <= 768) {
         $(".line_link").attr("href", "https://bit.ly/2YszXqD");
     } else {
         $(".line_link").attr("href", "https://line.me/R/ti/p/%40fhl1857j");
     }
-
-
+    
+    //週年慶活動辦法下拉選單
+    $(".anniversary_2,.SP_anniversary_2").hide();
+    $('.anniversary_1, .SP_anniversary_1').click(function (e) { 
+        e.preventDefault();
+        $(".anniversary_2,.SP_anniversary_2").toggle();;
+    });
+    $('.anniversary_2, .SP_anniversary_2').click(function (e) { 
+        e.preventDefault();
+        $(".anniversary_2, .SP_anniversary_2").hide();
+    });
 });
-
-// $("#lp_form").ready(function () {
-//     //購物車下拉選單判斷預購
-//     $('select[data-category="內衣"][data-name="size-selector"], select[data-category="內衣"][name="color-selector"]').change(function (e) {
-//         e.preventDefault();
-//         var size_selector = document.querySelector('select[data-category="內衣"][data-name="size-selector"]').value;
-//         var color_selector = document.querySelector('select[data-category="內衣"][name="color-selector"]').value;
-//         var preorder_text = $('<span>', { text: '(預購)', style: 'color: red;' });
-//         $('.form-group > span').empty();
-//         if ((color_selector == "天空藍" && size_selector == "M") || (color_selector == "櫻花粉" && size_selector == "M") || (color_selector == "櫻花粉" && size_selector == "ML") || (color_selector == "櫻花粉" && size_selector == "SM") || (color_selector == "海軍藍" && size_selector == "M") || (color_selector == "薰衣草紫" && size_selector == "M") || (color_selector == "寧靜灰" && size_selector == "S") || (color_selector == "異色黑" && size_selector == "S") || (color_selector == "異色黑" && size_selector == "ML")) {
-//             $('select[data-name="size-selector"]').parent().after(preorder_text);
-//         } else {
-//             $('.form-group > span').empty();
-//         }
-//     });
-//     // 天空藍：M
-//     // 櫻花粉：M、ML、SM
-//     // 海軍藍：M
-//     // 薰衣草紫：M
-//     // 寧靜灰: S
-//     // 異色黑: S、ML
-// });
